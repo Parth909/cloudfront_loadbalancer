66914541ca98c8004ddba62b34af2ebe449ae66b
Parth909 <parthbhoir909@gmail.com>
2022-07-05 12:30:49 +0530

    change the script for ubuntu

--- user_script.sh
+++ user_script.sh
@@ -1,25 +1,28 @@
 #!/bin/bash -ex
-# output user data logs into a separate file for debugging
+
+# Installing Node.js with Apt using NodeSource PPA
+
+sudo apt update
+
+sudo apt upgrade -y
+
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-# download nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-# source nvm
-. /.nvm/nvm.sh
-# install node
-nvm install node
-#export NVM dir
-export NVM_DIR="/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
-#upgrade yum
-sudo apt update && sudo apt upgrade
+cd ~
+
+curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
+
+sudo bash /tmp/nodesource_setup.sh
+
+sudo apt install nodejs
+
 #install git
 sudo apt install git -y
-cd /home/ec2-user
-# get source code from githubt
-git clone https://github.com/Parth909/cloudfront_loadbalancer
+cd /home/ubuntu
+
+# get source code from github
+git clone https://github.com/felixyu9/auto-scaling-nodejs-app
 #get in project dir
-cd cloudfront_loadbalancer
+cd auto-scaling-nodejs-app
 #give permission
 sudo chmod -R 755 .
 #install node module
